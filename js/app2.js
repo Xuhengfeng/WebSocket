bedae18947df65ed1fddbeb1185195b29c06f470
Xuhengfeng <564297479@qq.com>
2017-10-07 23:04:48 +0800

    修改app2.js服务

--- js/app2.js
+++ js/app2.js
@@ -4,6 +4,7 @@ var app = require('http').createServer();
 var io = require('socket.io')(app);
 
 const PORT = 3000;
+app.listen(PORT);
 let clientCount = 0;//客户端 数量
 
 io.on('connection', function (socket) {
@@ -11,15 +12,15 @@ io.on('connection', function (socket) {
   socket.nickName = 'user' + clientCount;
   //socket代表和客户端连接
   //io 代表广播 消息
-  io.emit('enter', conn.nickName + 'comes in');//派发事件到前端,发送数据对象到前端
+  io.emit('enter', socket.nickName + 'comes in');//派发事件到前端,发送数据对象到前端
   socket.on('message', function (str) {
-  	io.emit('enter', socket.nickname + 'says:' + str);
+  	io.emit('enter', socket.nickName + 'says:' + str);
   });
   socket.on('disconnect', function () {
-  	io.emit('left', socket.nickname + 'left');
+  	io.emit('left', socket.nickName + 'left');
+  	clientCount--;
   })
 });
-
 
 
 //在服务端打印log输出
